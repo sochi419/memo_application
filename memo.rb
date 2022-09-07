bc817b5b9d516fbba6a157d4a26b1c1673b92ad2
Soshi Ochiai <equipment2241@gmail.com>
2022-09-07 18:49:47 +0900

    Fixed the following points ・Change to processing using classes without using before

--- memo.rb
+++ memo.rb
@@ -10,13 +10,25 @@ helpers do
   end
 end
 
-before do
-  @memo_infos = []
-  @connect = PG.connect(host: 'localhost', dbname: 'mydb', port: '5432')
-  results = @connect.exec('SELECT * FROM books')
+class Memo
+  def initialize
+    @connect = PG.connect(host: 'localhost', dbname: 'mydb', port: '5432')
+  end
 
-  results.each do |result|
-    @memo_infos << result
+  def list
+    @connect.exec('select * from books order by id asc').to_a
+  end
+
+  def insert(new_memo_id, title, content)
+    @connect.exec('INSERT INTO books VALUES ($1, $2, $3)', [new_memo_id, title, content])
+  end
+
+  def update(title, content, id)
+    @connect.exec('update books set title=$1, body=$2 where id=$3', [title, content, id])
+  end
+
+  def delete(id)
+    @connect.exec('delete from books where id=$1', [id])
   end
 end
 
@@ -25,6 +37,9 @@ get '/' do
 end
 
 get '/memos' do
+  memo = Memo.new
+  @memos = memo.list
+
   erb :index
 end
 
@@ -33,54 +48,47 @@ get '/memos/new' do
 end
 
 post '/memos' do
+  memo = Memo.new
+  @memos = memo.list
+
   # 新規メモの、ID番号を決める処理。
-  new_memo_id = if @memo_infos == []
-                  1
-                else
-                  @memo_infos.max_by { |memo| memo['id'].to_i }['id'].to_i + 1
-                end
-
-  @connect.exec('INSERT INTO books VALUES ($1, $2, $3)', [new_memo_id, params['title'], params['content']])
-
-  results = @connect.exec('SELECT * FROM books')
-
-  results.each do |result|
-    @memo_infos << result
+  if @memos == []
+    new_memo_id = 1
+  else
+    id_aggregation = @memos.map { |data| data['id'].to_i }
+    new_memo_id = (id_aggregation.max + 1) # 「既存メモの最大ID + 1」 に新規メモのIDを設定する。
   end
+
+  memo.insert(new_memo_id, params['title'], params['content'])
 
   redirect "/memos/#{new_memo_id}"
 end
 
 get '/memos/:id' do
-  @memo_info = @memo_infos.find { |memo| memo['id'].to_i == params['id'].to_i }
+  memo = Memo.new
+  @memo = memo.list.find { |data| data['id'].to_i == params['id'].to_i }
 
   erb :detail
 end
 
 get '/memos/:id/edit' do
-  @memo_info = @memo_infos.find { |memo| memo['id'].to_i == params['id'].to_i }
+  memo = Memo.new
+  @memo = memo.list.find { |data| data['id'].to_i == params['id'].to_i }
   erb :edit
 end
 
 patch '/memos/:id' do
-  @connect.exec('update books set title=$1, body=$2 where id=$3', [params['title'], params['content'], params['id']])
-
-  results = @connect.exec('SELECT * FROM books')
-
-  results.each do |result|
-    @memo_infos << result
-  end
+  memo = Memo.new
+  memo.update(params['title'], params['content'], params['id'])
+  @memos = memo.list
 
   redirect "/memos/#{params['id']}"
 end
 
 delete '/memos/:id' do
-  @connect.exec("delete from books where id=#{params['id']}")
-  results = @connect.exec('SELECT * FROM books')
-
-  results.each do |result|
-    @memo_infos << result
-  end
+  memo = Memo.new
+  memo.delete(params['id'])
+  @memos = memo.list
 
   redirect '/'
 end
